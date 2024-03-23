f2a865915e5fd759d4207b1c327e6301d354c15e
Elisa Crescentini <elisa@ruggedsoftware.dev>
2024-03-23 17:28:10 +0100

    added HBF logo into navbar to link homepage

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -6,57 +6,73 @@ import LinkWrapper from "@/components/LinkWrapper";
 export default function DesktopNav() {
   return (
     <div className={`bg-white w-full`}>
-      <div className="mx-auto w-full max-w-screen-lg overflow-hidden px-5">
-        <ul className="flex items-center justify-center px-10 py-5 gap-10 text-xl font-normal font-happyMonkey">
-          <li>
+      <div className="w-full px-5">
+        <ul className="flex items-center px-10 py-5 text-xl font-normal font-happyMonkey w-full">
+          <li className="flex-1">
             <LinkWrapper
-              href="/breed"
+              href="/"
             >
-              Breed
+              <picture className="shrink-0">
+                <img
+                  src="/hbf-logo.svg"
+                  alt="Havana Brown Fanciers Logo"
+                  className="w-10"
+                />
+              </picture>
             </LinkWrapper>
           </li>
-          <li>
-            <LinkWrapper
-              href="/about"
-            >
-              About the Club
-            </LinkWrapper>
-          </li>
-          <li>
-            <LinkWrapper
-              href="/history"
-            >
-              History
-            </LinkWrapper>
-          </li>
-          <li>
-            <LinkWrapper
-              href="/breeders"
-            >
-              Breeders
-            </LinkWrapper>
-          </li>
-          <li>
-            <LinkWrapper
-              href="/show"
-            >
-              Show
-            </LinkWrapper>
-          </li>
-          <li>
-            <LinkWrapper
-              href="/photo"
-            >
-              Photo
-            </LinkWrapper>
-          </li>
-          <li>
-            <LinkWrapper
-              href="/news"
-            >
-              News
-            </LinkWrapper>
-          </li>
+          <ul className="flex items-center gap-10 flex-grow-0">
+            <li>
+              <LinkWrapper
+                href="/breed"
+              >
+                Breed
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/about"
+              >
+                About the Club
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/history"
+              >
+                History
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/breeders"
+              >
+                Breeders
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/show"
+              >
+                Show
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/photo"
+              >
+                Photo
+              </LinkWrapper>
+            </li>
+            <li>
+              <LinkWrapper
+                href="/news"
+              >
+                News
+              </LinkWrapper>
+            </li>
+          </ul>
+          <div className="flex-1"></div>
         </ul>
       </div>
     </div>
